f5d91f17e7b87a01637c1c3af20cacd2600fe088
Sebastian Werner <s.werner@sebastian-software.de>
2022-01-27 16:17:09 +0100

    Worked on tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,17 @@
+import { getRules } from "."
+
 describe("getRules", () => {
-  test
+  test("No Plugins", async () => {
+    const rules = await getRules({})
+    const keys = Array.from(rules.keys())
+    expect(keys.length).toBe(286)
+    expect(keys).toMatchSnapshot()
+  })
+
+  test("With JSDoc Plugin", async () => {
+    const rules = await getRules({ plugins: [ "jsdoc" ] })
+    const keys = Array.from(rules.keys())
+    expect(keys.length).toBe(334)
+    expect(keys).toMatchSnapshot()
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
-import fs from "fs"
 import path from "path"
-import eslintrc from "@eslint/eslintrc"
+import { Legacy } from "@eslint/eslintrc"
 import pkgDir from "pkg-dir"
 
-const builtInRules = require.resolve("eslint/lib/rules");
-
 export async function getRules(options) {
-    const configArrayFactory = new eslintrc.Legacy.CascadingConfigArrayFactory({
+    const configArrayFactory = new Legacy.CascadingConfigArrayFactory({
         cliConfig: { plugins: options.plugins },
         useEslintrc: false
     });
@@ -15,7 +12,8 @@ export async function getRules(options) {
     if (!root) {
       throw new Error("Installation Issue: ESLint package was not found!")
     }
-    const recommendedPath = join(root, "conf/eslint-recommended")
+    const rulesPath = path.join(root, "lib/rules")
+    const builtInRules = require(rulesPath)
 
 
     return new Map(function *() {
